b2b66ba2a9bb34c7f5288ea03e0aac2cfdecb140
codeDaiQin <1046572917@qq.com>
2023-03-31 23:28:59 +0800

    feat: 🙅🏻‍♀️ U test

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const apiKey = process.env.API_KEY ?? ''
 
 export const proxyUrl = ''
 
-export const groupKeywords: string[] = ['毛毛虫', '赛老师', '手冲']
+export const groupKeywords: string[] = []
 
 export default {
   apiKey,
